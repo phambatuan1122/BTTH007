5e666dacd7ba777d26a0e46e84f1d99baaccbddb
Phạm Bá Tuấn <95919510+CinXtea@users.noreply.github.com>
2022-03-28 17:38:18 +0700

    Phạm Bá Tuấn

--- script.js
+++ script.js
@@ -38,3 +38,54 @@ function leaveName(){
         statusOfName.style.color = 'red'
     }
 }
+function countryselect(ucountry) {
+    if (ucountry.value == "Default") {
+        alert('Chọn quốc gia của bạn từ danh sách');
+        ucountry.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+} 
+function allnumeric(uzip) {
+    var numbers = /^[0-9]+$/;
+    if (uzip.value.match(numbers)) {
+        return true;
+    }
+    else {
+        alert('ZIP code must have numeric characters only');
+        uzip.focus();
+        return false;
+    }
+}function ValidateEmail(uemail) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (uemail.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        uemail.focus();
+        return false;
+    }
+}
+
+function validsex(umsex, ufsex) {
+    x = 0;
+
+    if (umsex.checked) {
+        x++;
+    } if (ufsex.checked) {
+        x++;
+    }
+    if (x == 0) {
+        alert('Select Male/Female');
+        umsex.focus();
+        return false;
+    }
+    else {
+        alert('Form Successfully Submitted');
+        window.location.reload()
+        return true;
+    }
+}
